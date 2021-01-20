e640bf69123ff9ddbe82ea0fe6e280878a114020
Jay Lin <cooloojayoo@gmail.com>
2021-01-21 10:38:29 +1100

    App Crush

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
@@ -10,44 +10,45 @@ import { createStackNavigator } from '@react-navigation/stack';
 import DeckDetails from './DeckDetails';
 const Stack = createStackNavigator();
 
-const StackNavigator = () => (
-    <Stack.Navigator>
-        <Stack.Screen name="Deck1" component={DeckDetails} />
-    </Stack.Navigator>
-)
+// const StackNavigator = () => (
+//     <Stack.Navigator>
+//         <Stack.Screen name="Deck1" component={DeckDetails} />
+//     </Stack.Navigator>
+// )
 
-const Menu = () => {
-    return(
-     <View>
-       <Text>Hello</Text>
-     </View>)
-}
+// const Menu = () => {
+//     return(
+//      <View>
+//        <Text>Hello</Text>
+//      </View>)
+// }
 
-const Tab = createBottomTabNavigator();
-const TabNavigator = () => {
-    <Tab.Navigator
-        tabBarOptions={{
-            activeBackgroundColor: colors.primary,
-            activeTintColor: "white",
-            inactiveBackgroundColor: '#eee',
-            inactiveTintColor: "black"
-        }}
-    >
-        <Tab.Screen 
-            name="Menu" 
-            component={Menu} 
-            options={{
-                tabBarIcon: ({ size, color }) => <MaterialCommunityIcons name="home" size={size} color={color}/>
-            }}
-        />
-        <Tab.Screen name="AddDeck" component={AddDeck} />
-    </Tab.Navigator>
-}
+// const Tab = createBottomTabNavigator();
+// const TabNavigator = () => {
+//     <Tab.Navigator
+//         tabBarOptions={{
+//             activeBackgroundColor: colors.primary,
+//             activeTintColor: "white",
+//             inactiveBackgroundColor: '#eee',
+//             inactiveTintColor: "black"
+//         }}
+//     >
+//         <Tab.Screen 
+//             name="Menu" 
+//             component={Menu} 
+//             options={{
+//                 tabBarIcon: ({ size, color }) => <MaterialCommunityIcons name="home" size={size} color={color}/>
+//             }}
+//         />
+//         <Tab.Screen name="AddDeck" component={AddDeck} />
+//     </Tab.Navigator>
+// }
 
 export default function Home({ naigation }) {
     return (
         <View>
-            <TabNavigator />
+            {/* <TabNavigator /> */}
+           <Text>cccc</Text>
         </View>
     )
 }
